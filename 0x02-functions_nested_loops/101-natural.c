f1a838b51df21bff1fb0830d93f8ddb0efcb7bae
MahaseM <mahasemofokeng@yahoo.com>
2023-02-22 23:28:00 -0800

    Nature made natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,7 @@
 /**
  * main - program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded)
  * 
- * Return: Always 0 (Success)
+ * Return: Always (Success)
  */
 
 int main(void)
