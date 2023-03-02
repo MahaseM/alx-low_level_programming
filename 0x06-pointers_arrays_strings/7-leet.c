c01e5f534afac33c23a0d543143464522a793c8e
MahaseM <mahasemofokeng@yahoo.com>
2023-03-02 08:51:50 -0800

    Mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,17 +7,17 @@
 
 char *leet(char *n)
 {
-	int x, y;
+	int x, z;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
 	for (x= 0; n[x] != '\0'; x++)
 	{
-		for (y = 0; y < 10; y++)
+		for (z = 0; z < 10; z++)
 		{
-			if (n[x] == s1[y])
+			if (n[x] == s1[z])
 			{
-				[x] = s2[y];
+				[x] = s2[z];
 			}
 		}
 	}
